96c4043049d91213fbfceb1abf9e0b6f365700a0
A_Beille <78448220+A-Beille@users.noreply.github.com>
2022-09-29 18:45:07 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+//Code de l'algorithme. Vous pouvez l'utiliser pour vos projets personnels.
+function cryptincesar(wordte, encrypt){
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const word = wordte
+const encryptnmb = encrypt
+let finalarray = []
+word.toUpperCase().split('').forEach((crypted)=>{
+    let place = alphabet.indexOf(crypted)
+    let finalnmb = +place + +encryptnmb
+    let final = alphabet[finalnmb]
+    if(!final){
+    let errornb = encryptnmb-alphabet.reverse().indexOf(crypted)
+    alphabet.reverse()
+    final = alphabet[errornb-1]
+    }
+    finalarray.push(final)
+})
+return finalarray.join('').toString()
+}
+
+function decryptincesar(wordte, encrypt){
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    alphabet.reverse()
+    const word = "LKXKXO"
+    for(let encryptnmb = 1;encryptnmb<=25;encryptnmb++){
+    let finalarray = []
+    word.toUpperCase().split('').forEach((crypted)=>{
+        let place = alphabet.indexOf(crypted)
+        let finalnmb = +place + +encryptnmb
+        let final = alphabet[finalnmb]
+        if(!final){
+        let errornb = encryptnmb-alphabet.reverse().indexOf(crypted)
+        alphabet.reverse()
+        final = alphabet[errornb-1]
+        }
+        finalarray.push(final)
+    })
+    console.log(finalarray.join('').toString())
+}
+    }
+    decryptincesar()
